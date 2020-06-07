ca2177a2ba216d952e17d39a270ac548f15527a1
Guillaume Moreau <guillaume.moreau@ec-nantes.fr>
2020-06-07 19:20:51 +0200

    exemple kosaraju

--- snippets/kosaraju.py
+++ snippets/kosaraju.py
@@ -6,29 +6,94 @@ import mygraphs
 
 
 # chargement du graphe
-g1, visual_style = mygraphs.build_graph_exemple_24(True)
+g1, visual_style = mygraphs.build_graph_exemple_24dense(True)
+visual_style['canvas'] = (9, 6)
+color_dict = {0: 'blue', 1: 'red', 2: 'green', -1: 'black', 3: 'magenta', 4:'yellow', 5: 'cyan',
+              6: 'purple', 7: 'brown', 8: 'lime', 9: 'teal', 10: 'red', 11: 'darkgray', 12: 'gray',
+              13: 'pink', 14: 'violet', 15: 'orange', 16: 'black'}
+
+
+gn = 1
+
+def export_graphe(graphe):
+    global gn
+    filename = 'kosaraju-{cc}.tex'.format(cc=gn)
+    visual_style['vertex_color'] = [color_dict[g] for g in graphe.vs["vu"]]
+    plot(graphe, filename, **visual_style)
+    gn = gn+1
+
 
 # Parcours en profondeur avec stockage de l'ordre suffixe 
-
-
-def visite(i):
+def visite(g,i):
     global ordre_suffixe
+    global counter
     i["vu"] = True
-    for j in g1.successors(i):
-        if g1.vs[j]["vu"] == False:
-            visite(g1.vs[j])
+    i["date"] = str(counter)
+    counter = counter + 1
+    # à chaque fois qu'on visite un sommet, on exporte le graphe
+    visual_style['node_label'] = g.vs["date"]
+    export_graphe(g)
+    for j in g.successors(i):
+        if g.vs[j]["vu"] == False:
+            visite(g,g.vs[j])
     ordre_suffixe.append(i["nom"])
 
+
+def DFSUtil(ginv, v, ncb):
+    v["vu"] = True
+    v["comp"] = ncb
+    export_graphe(ginv)
+    for i in v.successors():
+        if i["vu"] == False:
+            DFSUtil(ginv, i, ncb)
+
+# initialisation
 counter = 1
 for s in g1.vs:
     s["vu"] = False
     s["nom"] = str(counter)
+    s["date"] = ""
     counter = counter + 1
+visual_style['node_label'] = g1.vs["nom"]
 ordre_suffixe = []
+# sauvegarde du graphe originel
+plot(g1, 'kosaraju-0.tex', **visual_style)
+counter = 1
 for i in g1.vs:
     if i["vu"] == False:
-        visite(i)
-print(ordre_suffixe)
+        visite(g1,i)
 
-    visual_style['node_label'] = g1.vs["nom"]
-plot(g1,'kosaraju-1.tex',**visual_style)
+print(ordre_suffixe)
+plot(g1,'kosaraju-p1.tex',**visual_style)
+
+
+#inversion du graphe 
+ginv = ig.Graph(24,[],True)
+n = 1
+for v in ginv.vs:
+    v["nom"] = str(n) 
+    v["vu"] = False 
+    v["date"] = ""
+    n = n + 1
+for e in g1.es: 
+    ginv.add_edge(e.target,e.source)
+
+visual_style['node_label'] = ginv.vs["nom"]
+plot(ginv, 'kosaraju-inverse.tex', **visual_style)
+
+
+ncb = 1
+
+while ordre_suffixe:
+    i = ordre_suffixe.pop()
+    e = ginv.vs.select(nom = i)[0]
+    if (e["vu"] == False):
+        e["comp"] = ncb
+        DFSUtil(ginv,e,ncb)
+        ncb = ncb+1
+        print("/") 
+
+
+visual_style['node_label'] = ginv.vs["comp"]
+visual_style['vertex_color'] = [color_dict[g] for g in ginv.vs["comp"]]
+plot(ginv, 'kosaraju-cfc.tex', **visual_style)
--- snippets/mygraphs.py
+++ snippets/mygraphs.py
@@ -1,6 +1,24 @@
 import igraph as ig
 from network2tikz import plot
 
+# petit graphe à 8 sommets pour les CFC 
+def build_graph_exemple_8(oriente = True):
+    g1 = ig.Graph(8,[(0,2),(1,2),(2,3),(3,2),(4,1),(1,4),(1,5),(5,6),(6,5),(2,6),(6,7),(3,7)],oriente)
+    visual_style = {}
+    visual_style['edge_width'] = 2
+    visual_style['node_size'] = .8
+    visual_style['standalone'] = False
+    visual_style['node_opacity'] = 0.5
+
+    # layout grille
+    layout = {}
+    for y in range(0, 1):
+        for x in range(0, 3):
+            layout[y*4+x] = (x, -y)
+    visual_style['layout'] = layout
+
+    return g1,visual_style
+
 # construction du graphe exemple tiré du cours de DP
 def build_graph_exemple_24(oriente = False):
     g1 = ig.Graph(24, [(0, 6), (6, 12), (12, 18), (7, 13), (13, 19), (2, 8), (8, 14), (3, 9), (15, 21), (4, 10), (10, 16), (5, 11), (11, 17),
@@ -32,6 +50,41 @@ def build_graph_exemple_24(oriente = False):
 
     # print(g1.vs["name"])
     return g1,visual_style
+
+
+# construction du graphe exemple tiré du cours de DP
+def build_graph_exemple_24dense(oriente=False):
+    g1 = ig.Graph(24, [(0, 6), (6, 12), (12, 18), (7, 13), (19, 13), (2, 8), (14, 8), (3, 9), (15, 21), (4, 10), (10, 16), (5, 11), (11, 17),
+                       (6, 7), (8, 7), (8, 9), (9, 10), (12, 13), (13, 14), (16, 17), (18, 19), (20, 21), (22, 23),
+                       (8,2),(13,6)], oriente)
+    visual_style = {}
+    visual_style['edge_width'] = 2
+    visual_style['node_size'] = .8
+    visual_style['standalone'] = False
+    visual_style['node_opacity'] = 0.5
+
+    # layout grille
+    layout = {}
+    for y in range(0, 4):
+        for x in range(0, 6):
+            layout[y*6+x] = (x, -y)
+    visual_style['layout'] = layout
+
+    # numérotation des sommets
+    # counter = 1
+    # g1.vs["name"] = ["1"]
+    # first = True
+    # for s in g1.vs:
+    #     if first:
+    #         first = False
+    #     else:
+    #         g1.vs["name"].append(str(counter))
+    #         print(counter)
+    #     counter = counter + 1
+
+    # print(g1.vs["name"])
+    return g1, visual_style
+
 
 def build_graph_exemple_6():
     g1 = ig.Graph(6, [(0, 1), (0, 3), (1, 3), (2, 8),
