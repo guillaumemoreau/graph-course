a91913df156edf6393df647151db8f1b5f38d5f3
Guillaume Moreau <guillaume.moreau@ec-nantes.fr>
2020-06-18 17:28:06 +0200

    plus courts chemins : Dijkstra

--- snippets/dijkstra.py
+++ snippets/dijkstra.py
@@ -1,56 +1,50 @@
+# algorithme de Diskstra 
+
 import igraph as ig 
 from network2tikz import plot
 import mygraphs 
 import math 
 
-# construction du tri topologique 
-def tri_topologique(G):
-    listeSommets = []
-    for i in G.vs:
-        i["col"] = 0 # 0 pour blanc, 1 pour gris, 2 pour noir 
-    
-    for x in G.vs:
-        if x["col"] == 0:
-            pp_topologique(G,listeSommets,x)
 
-    return listeSommets
-
-def pp_topologique(G, listeSommets, x):
-    x["col"] = 1
-    for i in x.successors():
-        if i["col"] == 0:
-            pp_topologique(G,listeSommets,i)
-    
-    x["col"] = 2
-    listeSommets.append(x)
-
-def ordinal(G,s):
-    ord = tri_topologique(G)
+def dijkstra(G,s):
     for i in G.vs:
         i["dist"] = math.inf
         i["pred"] = ""
-    while ord:
-        i = ord.pop()
-        print("processing "+i["name"])
-        if (i == s):
-            i["dist"] = 0
+        i["done"] = False 
+    s["dist"] = 0
+    pas_fini = True 
+    while pas_fini:
+        mcout = math.inf
+        msommet = s 
+        for i in G.vs:
+            if i["done"] == False:
+                if (i["dist"] < mcout):
+                    mcout = i["dist"]
+                    msommet = i 
+        if (mcout < math.inf):
+            msommet["done"] = True 
+            for j in msommet.successors():
+                if msommet["dist"]+G.es[G.get_eid(msommet,j)]["w"] < j["dist"]:
+                    j["dist"] = msommet["dist"]+G.es[G.get_eid(msommet,j)]["w"]
+                    j["pred"] = msommet
         else:
-            for j in i.predecessors():
-                if j["dist"] + G.es[G.get_eid(j,i)]["w"] < i["dist"]:
-                    i["dist"] = j["dist"] + G.es[G.get_eid(j,i)]["w"]
-                    i["pred"] = j
+            pas_fini = False 
         # affichage des résultats 
-        str_names = ""
+        str_names = " & "
         str_dist = "\\texttt{dist} & "
         str_pred = "\\texttt{pred} &"
         for x in G.vs:
-            str_names = str_names + x["name"] + '\t&'
+            if x["done"]:
+                str_names = str_names + '\\textbf{' + x["name"] + '}\t&'
+            else:
+                str_names = str_names + x["name"] + '\t&'
             str_dist = str_dist + str(x["dist"]) + '\t&'
             if x["pred"] == "":
                 str_pred = str_pred + '\t&'
             else:
                 str_pred = str_pred + x["pred"]["name"] + '\t&'
         print(str_names)
+        print('\hline')
         print(str_pred)
         print(str_dist)
         print('----------------------------------------------')
@@ -76,8 +70,5 @@ layout = {
     7: (6,-4)
 }
 visual_style['layout'] = layout
-plot(g,'ordinal-0.pdf',**visual_style)
-ordinal(g,g.vs.find(name="s"))
-
-
-
+plot(g,'dijkstra-0.pdf',**visual_style)
+dijkstra(g,g.vs.find(name="s"))
